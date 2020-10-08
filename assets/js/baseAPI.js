d74881d402a2cc3d9ae06a373b8f04a0d95591c9
jixiang <15234258020@163.com>
2020-10-08 16:07:43 +0800

    首页开发完成

--- assets/js/baseAPI.js
+++ assets/js/baseAPI.js
@@ -5,7 +5,25 @@
 // 拦截所有的ajax请求
 // option 参数，获取所有的配置对象
 $.ajaxPrefilter(function (option) { 
-    alert(option.url);
+    
     option.url = 'http://ajax.frontend.itheima.net' + option.url;
-    alert(option.url);
+    
+    // 对需要权限的服务器地址配置头信息
+    // 必须一my开头才行
+    if (option.url.indexOf('/my/') !== -1) { 
+        option.headers = {
+            Authorization:localStorage.getItem('token') || ''
+        }
+    }
+
+    option.complete = function (res) { 
+        console.log(res);
+        // 判断 如果是身份认证失败，跳转到登录页面
+        if (res.responseJSON.status == 1 && res.responseJSON.message == '身份认证失败！') { 
+            // 清空本地 token
+            localStorage.removeItem('token');
+            // 页面跳转
+            location.href = '/login.html';
+        }
+    }
 })
